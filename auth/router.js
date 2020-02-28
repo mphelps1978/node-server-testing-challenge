d3d93b5bc9ecb047dd9cd504d880524e7bf1f072
Michael Phelps <michaeljphelps1978@gmail.com>
2020-02-27 20:26:27 -0600

    Bugfix. Server Sanity Check passes in Insomnia

--- auth/router.js
+++ auth/router.js
@@ -3,3 +3,5 @@ const router = require('express').Router()
 router.get('/', (req, res) => {
   res.status(200).json({route: 'auth'})
 })
+
+module.exports = router
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require ('./api/server')
 
 const PORT = process.env.PORT || 5000
 
-server.listen(port, () => {
+server.listen(PORT, () => {
   console.log(`\n*** Listening better than my kids on ${PORT} ***\n` );
 
 })
